1bc16f3268049fc378fe4f61c551d2d6e8bda0f1
Dmytro Domshyn <dmytro.domshyn@storytel.com>
2020-12-29 16:51:04 +0100

    Day 3 and refactor project structure

--- 2019/solutions/solution.go
+++ 2019/solutions/solution.go
@@ -3,6 +3,10 @@ package solutions
 import (
 	"fmt"
 	"time"
+
+	"github.com/domszyn/adventofcode/2019/solutions/day1"
+	"github.com/domszyn/adventofcode/2019/solutions/day2"
+	"github.com/domszyn/adventofcode/2019/solutions/day3"
 )
 
 type Solution struct {
@@ -14,4 +18,33 @@ type Solution struct {
 func (s *Solution) Print() {
 	fmt.Printf("% +v", *s)
 	fmt.Println()
+}
+
+func (s *Solution) SolveDay1() {
+	start := time.Now()
+	s.Part1 = day1.FuelRequirement(false)
+	s.Part2 = day1.FuelRequirement(true)
+	s.ExecTime = time.Since(start)
+	s.Print()
+}
+
+func (s *Solution) SolveDay2() {
+	start := time.Now()
+
+	s.Part1 = day2.SolvePart1()
+	s.Part2 = day2.SolvePart2()
+
+	s.ExecTime = time.Since(start)
+	s.Print()
+}
+
+func (s *Solution) SolveDay3() {
+	start := time.Now()
+	answers := day3.GetAnswers()
+
+	s.Part1 = answers[0]
+	s.Part2 = answers[1]
+
+	s.ExecTime = time.Since(start)
+	s.Print()
 }
