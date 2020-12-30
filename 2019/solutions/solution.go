fd6361adcf5c33b58759c2365b9b98bce4c3881c
Dmytro Domshyn <dmytro.domshyn@storytel.com>
2020-12-30 19:37:08 +0100

    Day 7 and more IntCode refactoring

--- 2019/solutions/solution.go
+++ 2019/solutions/solution.go
@@ -10,6 +10,7 @@ import (
 	"github.com/domszyn/adventofcode/2019/solutions/day4"
 	"github.com/domszyn/adventofcode/2019/solutions/day5"
 	"github.com/domszyn/adventofcode/2019/solutions/day6"
+	"github.com/domszyn/adventofcode/2019/solutions/day7"
 )
 
 type Solution struct {
@@ -65,6 +66,13 @@ func (s *Solution) SolveDay5() {
 func (s *Solution) SolveDay6() {
 	start := time.Now()
 	s.Part1, s.Part2 = day6.GetAnswers()
+	s.ExecTime = time.Since(start)
+	s.Print()
+}
+
+func (s *Solution) SolveDay7() {
+	start := time.Now()
+	s.Part1, s.Part2 = day7.GetAnswers()
 	s.ExecTime = time.Since(start)
 	s.Print()
 }
--- 2019/toolbox/intcode_test.go
+++ 2019/toolbox/intcode_test.go
@@ -1,6 +1,8 @@
 package toolbox
 
-import "testing"
+import (
+	"testing"
+)
 
 func assertProgram(t *testing.T, program Program, input, output, expected int) {
 	if output != expected {
@@ -9,85 +11,144 @@ func assertProgram(t *testing.T, program Program, input, output, expected int) {
 }
 
 func TestEqualTo8UsingPositionMode(t *testing.T) {
-	program := []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}
-	input := 8
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1)
+	var program Program = []int{3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8}
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 1)
+	}
 
-	input = 0
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 0)
+	input <- 0
+	select {
+	case result := <-output:
+		assertProgram(t, program, 0, result, 0)
+	}
 }
 
 func TestLessThan8UsingPositionMode(t *testing.T) {
-	program := []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}
-	input := 0
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1)
+	var program Program = []int{3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8}
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 0)
+	}
 
-	input = 8
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 0)
+	input <- 0
+	select {
+	case result := <-output:
+		assertProgram(t, program, 0, result, 1)
+	}
 }
 
 func TestEqualTo8UsingImmediateMode(t *testing.T) {
-	program := []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}
-	input := 8
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1)
+	var program Program = []int{3, 3, 1108, -1, 8, 3, 4, 3, 99}
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 1)
+	}
 
-	input = 0
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 0)
+	input <- 0
+	select {
+	case result := <-output:
+		assertProgram(t, program, 0, result, 0)
+	}
 }
 
 func TestLessThan8UsingImmediateMode(t *testing.T) {
-	program := []int{3, 3, 1107, -1, 8, 3, 4, 3, 99}
-	input := 0
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1)
+	var program Program = []int{3, 3, 1107, -1, 8, 3, 4, 3, 99}
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 0)
+	}
 
-	input = 8
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 0)
+	input <- 0
+	select {
+	case result := <-output:
+		assertProgram(t, program, 0, result, 1)
+	}
 }
 
 func TestJumpUsingPositionMode(t *testing.T) {
-	program := []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
-	input := 0
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 0)
+	var program Program = []int{3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9}
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 1)
+	}
 
-	input = 5
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1)
+	input <- 0
+	select {
+	case result := <-output:
+		assertProgram(t, program, 0, result, 0)
+	}
 }
 
 func TestJumpUsingImmediateMode(t *testing.T) {
-	program := []int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}
-	input := 0
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 0)
+	var program Program = []int{3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1}
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 1)
+	}
 
-	input = 5
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1)
+	input <- 0
+	select {
+	case result := <-output:
+		assertProgram(t, program, 0, result, 0)
+	}
 }
 
 func TestCombined(t *testing.T) {
-	program := []int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+	var program Program = []int{3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
 		1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
 		999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99}
 
-	input := 7
-	output := IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 999)
+	input := make(chan int)
+	output := make(chan int)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	go program.IntCode(input, output, nil)
+	input <- 7
+	select {
+	case result := <-output:
+		assertProgram(t, program, 7, result, 999)
+	}
 
-	input = 8
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1000)
+	input <- 8
+	select {
+	case result := <-output:
+		assertProgram(t, program, 8, result, 1000)
+	}
 
-	input = 9
-	output = IntCodeWithInput(program, []Replacement{}, input)
-	assertProgram(t, program, input, output, 1001)
+	input <- 9
+	select {
+	case result := <-output:
+		assertProgram(t, program, 9, result, 1001)
+	}
 }
