42f2418b82bad32e63e0d3acd645aad0ce3ba023
Dmytro Domshyn <dmytro.domshyn@storytel.com>
2021-12-01 12:13:33 +0100

    Day 1

---
+++ 2021/go.mod
@@ -0,0 +1,3 @@
+module github.com/domszyn/adventofcode/2021
+
+go 1.17
---
+++ 2021/solutions/solution.go
@@ -0,0 +1,27 @@
+package solutions
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/domszyn/adventofcode/2021/solutions/day1"
+)
+
+type Solution struct {
+	Part1    interface{}
+	Part2    interface{}
+	ExecTime time.Duration
+}
+
+func (s *Solution) Print() {
+	fmt.Printf("% +v", *s)
+	fmt.Println()
+}
+
+func (s *Solution) SolveDay1() {
+	start := time.Now()
+	s.Part1 = day1.SolvePart1()
+	s.Part2 = day1.SolvePart2()
+	s.ExecTime = time.Since(start)
+	s.Print()
+}
