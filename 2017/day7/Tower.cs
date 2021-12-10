fb660bc1caad6af14ad7ebb808dba451d4bc5505
Dmytro Domshyn <dmytro.domshyn@storytel.com>
2021-12-10 17:17:27 +0100

    Day 7

---
+++ 2017/day7/Tower.cs
@@ -0,0 +1,26 @@
+public class Tower
+{
+    private List<Tower> disc = new List<Tower>();
+    public string Name { get; set; } = "";
+    public int OwnWeight { get; set; } = 0;
+    public IEnumerable<Tower> Disc => disc.AsReadOnly();
+
+    public int TotalWeight => OwnWeight + disc.Select(t => t.TotalWeight).Sum();
+
+    public Tower UnbalancedSubTower()
+    {
+        if (disc.Count < 2)
+        {
+            return null;
+        }
+
+        var outliers = disc.Skip(1).Where( t => t.TotalWeight != disc[0].TotalWeight);
+
+        return outliers.Count() > 1 ? disc[0] : outliers.SingleOrDefault();
+    }
+
+    public void AddTower(Tower tower)
+    {
+        disc.Add(tower);
+    }
+}
