6ec79959000e461e71b777dd6fa91a69c7810a53
Dmytro Domshyn <dmytro.domshyn@storytel.com>
2022-12-03 06:18:16 +0100

    Day 3

--- 2022/solutions/day03/solution.go
+++ 2022/solutions/day03/solution.go
@@ -1,12 +1,56 @@
 package day03
 
 import (
+	"strings"
+
 	"github.com/domszyn/adventofcode/2022/mappers"
 	"github.com/domszyn/adventofcode/2022/utils"
 )
 
-func Solve() (int, int) {
+func count(item rune) int32 {
+	if item >= 'a' && item <= 'z' {
+		return item - 'a' + 1
+	}
+
+	if item >= 'A' && item <= 'Z' {
+		return item - 'A' + 27
+	}
+
+	return 0
+}
+
+func Solve() (part1 int32, part2 int32) {
 	lines := utils.ReadInput("./solutions/day03/input.txt", mappers.ToString)
 
-	return len(lines), len(lines)
+	for _, rucksack := range lines {
+		comp1, comp2 := rucksack[:len(rucksack)/2], rucksack[len(rucksack)/2:]
+
+		for _, item := range comp1 {
+			if strings.ContainsRune(comp2, item) {
+				part1 += count(item)
+				break
+			}
+		}
+	}
+
+	var groups [][]string
+	for i, v := range lines {
+		if i%3 == 0 {
+			groups = append(groups, make([]string, 0, 3))
+		}
+
+		groups[len(groups)-1] = append(groups[len(groups)-1], v)
+	}
+
+	for _, g := range groups {
+		for _, item := range g[0] {
+			if strings.ContainsRune(g[1], item) && strings.ContainsRune(g[2], item) {
+				part2 += count(item)
+				break
+			}
+		}
+
+	}
+
+	return part1, part2
 }
