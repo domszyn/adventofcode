046319425d6e39c56fdca68f5fc249b0b3c2aa08
Dmytro Domshyn <dmytro.domshyn@storytel.com>
2022-12-11 05:59:47 +0100

    Day 10

--- 2022/solutions/day10/solution.go
+++ 2022/solutions/day10/solution.go
@@ -1,12 +1,54 @@
 package day10
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/domszyn/adventofcode/2022/mappers"
 	"github.com/domszyn/adventofcode/2022/utils"
 )
 
-func Solve() (int, int) {
+func signalStrength(numbers []int, numCycles int) int {
+	return utils.Sum(numbers[:numCycles-1]) + 1
+}
+
+func Solve() (part1 int, part2 string) {
 	lines := utils.ReadInput("./solutions/day10/input.txt", mappers.ToString)
 
-	return len(lines), len(lines)
+	var numbers []int
+	for _, v := range lines {
+		if v == "noop" {
+			numbers = append(numbers, 0)
+			continue
+		}
+
+		var n int
+		fmt.Sscanf(v, "addx %d", &n)
+		numbers = append(numbers, 0)
+		numbers = append(numbers, n)
+	}
+
+	for i := 20; i <= 220; i += 40 {
+		part1 += i * signalStrength(numbers, i)
+	}
+
+	var output []string
+
+	for i := 0; i < 6; i++ {
+		output = append(output, strings.Repeat(".", 40))
+	}
+
+	for i := 0; i < 240; i++ {
+		x := i % 40
+		y := i / 40
+
+		s := signalStrength(numbers, i+1)
+		if s >= x-1 && s <= x+1 {
+			output[y] = output[y][:x] + "#" + output[y][x+1:]
+		}
+	}
+
+	part2 = "\n\n" + strings.Join(output, "\n")
+
+	return
 }
